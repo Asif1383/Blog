0fd9c3e5a1618988593f0379ab49750b759d1f15
Asif Khan <asif118052002@gmail.com>
2022-06-26 12:38:11 +0530

    Intial Commit change 6

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import os
 
 is_logged_in = False
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'sqlite:///blog.db')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -226,14 +226,12 @@ def edit_post(post_id):
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        author=post.author,
         body=post.body
     )
     if edit_form.validate_on_submit():
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
